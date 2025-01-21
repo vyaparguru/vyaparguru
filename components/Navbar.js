287aaefdc80ec8f653f2e4cfa87cf191b65307ed
relish <guptarelish333@gmail.com>
2025-01-22 00:42:53 +0530

    added dropdowns

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,159 +1,230 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { CiHeart, CiPhone, CiSearch, CiShoppingCart, CiUser } from "react-icons/ci";
+import {
+  CiHeart,
+  CiPhone,
+  CiSearch,
+  CiShoppingCart,
+  CiUser,
+} from "react-icons/ci";
+import { FiMenu, FiX, FiArrowRight } from "react-icons/fi";
+import { PiPhoneCallLight } from "react-icons/pi";
+import { MdOutlineKeyboardArrowRight, MdOutlinePhoneInTalk } from "react-icons/md";
 import logo from "../assets/logo.png";
+import { FaBarsStaggered, FaPhone } from "react-icons/fa6";
 
 const Navbar = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchDropdown,setSearchDropdown]=useState(false)
+  const [openDropdown, setOpenDropdown] = useState(null); 
 
   const navLinks = [
-    { name: "Marketing", href: "/collections/marketing" },
-    { name: "eCommerce", href: "/collections/ecommerce" },
-    { name: "IT Services", href: "/collections/it-services" },
-    { name: "Content Creation", href: "/collections/content-creation" },
-    { name: "Business Set-up", href: "/collections/business-setup" },
-    { name: "Legal, Book Keeping and Compliance Services", href: "/collections/legal, book keeping & compliance services" },
+    {
+      name: "Marketing",
+      href: "/collections/marketing",
+      subItems: ["Digital Marketing", "Social Media", "SEO"],
+    },
+    {
+      name: "eCommerce",
+      href: "/collections/ecommerce",
+      subItems: ["Online Stores", "Payment Gateways"],
+    },
+    {
+      name: "IT Services",
+      href: "/collections/it-services",
+      subItems: ["Web Development", "App Development"],
+    },
+    {
+      name: "Content Creation",
+      href: "/collections/content-creation",
+      subItems: ["Video Production", "Graphic Design"],
+    },
+    {
+      name: "Business Set-up",
+      href: "/collections/business-setup",
+      subItems: ["Company Registration", "Business Planning"],
+    },
+    {
+      name: "Legal, Book Keeping and Compliance Services",
+      href: "/collections/legal, book keeping & compliance services",
+      subItems: ["Tax Compliance", "Auditing", "Legal Advisory"],
+    },
   ];
-
-  const services = [
-    "Digital Marketing",
-    "WhatsApp Marketing",
-    "Online Selling",
-    "Payment Gateways",
-    "Website Designing",
-  ];
-
-  // Filter services based on search query
-  const filteredServices = services.filter((service) =>
-    service.toLowerCase().includes(searchQuery.toLowerCase())
-  );
 
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
-    setIsSearchOpen(true); // Open the dropdown when typing
   };
 
-  const handleServiceClick = (service) => {
-    const serviceUrl = service.replace(/\s+/g, "-").toLowerCase();
-    window.location.href = `/services/${serviceUrl}`;
+  const filteredServices = navLinks
+    .flatMap((link) => link.subItems || [])
+    .filter((service) =>
+      service.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+  const toggleDropdown = (index) => {
+    setOpenDropdown(openDropdown === index ? null : index);
   };
 
-  const handleBlur = () => {
-    // Close the dropdown with a slight delay to allow click events on dropdown items
-    setTimeout(() => setIsSearchOpen(false), 200);
-  };
+  const toggleSearchDropdown=()=>{
+    setSearchDropdown(true)
+    setIsMobileMenuOpen(false)
+  }
+
+
   return (
     <nav className="navbar bg-white shadow-md fixed w-full z-50 py-2">
-  <div className="flex max-w-screen-xl mx-auto px-6">
-    {/* Top Row: Logo, Search Box, and Icons */}
-    <div className="flex items-center justify-between">
-      {/* Logo */}
-      <div className="flex-shrink-0">
-        <Link href="/">
-          <Image
-            src={logo}
-            alt="VyaparGuru-Logo"
-            width={230}
-            height={40}
-            className="h-auto"
-          />
-        </Link>
-      </div>
+      <div className="flex justify-between items-center max-w-screen-xl mx-auto px-2 lg:px-10">
+        <div className="flex items-center z-50">
+          <button
+            className="lg:hidden p-2 rounded-full"
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          >
+            {isMobileMenuOpen ? <FiX className="w-6 h-6 z-50" /> : <FaBarsStaggered className="w-6 h-6" />}
+          </button>
+          <Link href="/">
+            <Image src={logo} alt="VyaparGuru-Logo" width={160} height={40} className="h-auto" />
+          </Link>
+        </div>
 
-      {/* Search Box */}
-      <div className="flex flex-col mx-4">
-
-      <div className="flex">
-        <div className="relative w-full">
-          <CiSearch className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500 w-6 h-6" />
-          <input
-            type="text"
-            placeholder="Search for services"
-            value={searchQuery}
+        {/* Desktop Search */}
+        <div className="hidden lg:flex flex-1 mx-4 z-10">
+          <div className="relative w-full">
+            <CiSearch className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500 w-6 h-6" />
+            <input
+              type="text"
+              placeholder="Search for services"
+              value={searchQuery}
               onChange={handleSearch}
-              onFocus={() => setIsSearchOpen(true)}
-              onBlur={handleBlur}
-            className="w-full pl-12 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600"
-          />
-        </div>
-
-        {isSearchOpen && filteredServices.length > 0 && (
-            <div className="absolute mx-8 bg-white border rounded-lg shadow-lg mt-12 max-h-60 overflow-y-auto z-50">
-            {filteredServices.map((service, index) => (
-              <div
-                key={index}
-                className="px-4 py-2 hover:bg-blue-100 cursor-pointer text-gray-700"
-                onMouseDown={() => handleServiceClick(service)} // Prevent blur on click
-              >
-                {service}
-              </div>
-            ))}
-          </div>
-          )}
-       
-
-      
-
-      {/* Icons */}
-      <div className="flex items-center space-x-1 pl-1">
-        <button className="p-2 rounded-full hover:bg-blue-100">
-          <CiPhone className="w-6 h-6" />
-        </button>
-        <button className="p-2 rounded-full hover:bg-blue-100">
-          <CiUser className="w-6 h-6"/>
-        </button>
-        <button className="p-2 rounded-full hover:bg-blue-100">
-          <CiHeart className="w-6 h-6" />
-        </button>
-        <button className="p-2 rounded-full hover:bg-blue-100">
-          <CiShoppingCart className="w-6 h-6" />
-        </button>
-      </div>
-    </div>
-   
-
-    {/* Bottom Row: Menu Items */}
-    <div className="mt-4 flex justify-start">
-          <div className="flex space-x-5">
-            {navLinks.map((navlink, index) => {
-              const isLast = index === navLinks.length - 1;
-              if (!isLast) {
-                const [firstWord, ...restWords] = navlink.name.split(" ");
-                return (
-                  <Link
-                    key={navlink.name}
-                    href={navlink.href}
-                    className="flex items-center text-center text-black p-2 rounded-xl bg-blue-100 text-sm"
+              className="w-full pl-12 py-2 border rounded-full cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-600"
+              onClick={toggleSearchDropdown}
+            />
+            {searchDropdown && filteredServices.length > 0 && (
+              <div className="absolute bg-white border rounded-lg shadow-lg mt-2 max-h-60 overflow-y-auto z-50 w-full">
+                {filteredServices.map((service, index) => (
+                  <div
+                    key={index}
+                    className="px-4 py-2 hover:bg-blue-100 cursor-pointer text-gray-700"
                   >
-                    {firstWord}{" "}
-                    {/* <br/> */}
-                    {restWords.join(" ")}
-                  </Link>
-                );
-              }
-              return (
-                <Link
-                  key={navlink.name}
-                  href={navlink.href}
-                  className="text-center text-black p-2 rounded-xl bg-blue-100 text-sm"
-                >
-                  Legal, Book Keeping 
-                  and Compliance Services
-                </Link>
-                
-              );
-            })}
-          </div> 
+                    {service}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
+
+        {/* Icons */}
+        <div className="flex items-center space-x-1">
+          <button className="p-2 rounded-full hover:bg-gray-100 hidden lg:block">
+            <CiPhone className="w-6 h-6" />
+          </button>
+          <button className="p-2 rounded-full hover:bg-gray-100 hidden lg:block">
+            <CiUser className="w-6 h-6 " />
+          </button>
+          <button
+            className="p-2 rounded-full hover:bg-gray-100 block lg:hidden"
+            onClick={() => {
+              setIsSearchOpen(!isSearchOpen)
+              setIsMobileMenuOpen(false)
+            }}
+          >
+            <CiSearch className="w-6 h-6" />
+          </button>
+          <button className="p-2 rounded-full hover:bg-gray-100">
+            <CiHeart className="w-6 h-6" />
+          </button>
+          <button className="p-2 rounded-full hover:bg-gray-100">
+            <CiShoppingCart className="w-6 h-6" />
+          </button>
         </div>
       </div>
-  </div>
-</nav>
-  )
+
+      {/* Mobile Search */}
+      {isSearchOpen && (
+        <div className="lg:hidden px-4 py-2">
+          <div className="relative">
+            <CiSearch className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500 w-6 h-6" />
+            <input
+              type="text"
+              placeholder="Search for services"
+              value={searchQuery}
+              onChange={handleSearch}
+              className="w-full pl-12 py-2 border cursor-pointer rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600"
+              onClick={toggleSearchDropdown}
+            />
+          </div>
+          {searchDropdown && filteredServices.length > 0 && (
+              <div className="absolute bg-white border rounded-lg shadow-lg mt-2 max-h-60 overflow-y-auto z-5 w-full">
+                {filteredServices.map((service, index) => (
+                  <div
+                    key={index}
+                    className="px-4 py-2 hover:bg-blue-100 cursor-pointer text-gray-700"
+                  >
+                    {service}
+                  </div>
+                ))}
+              </div>
+            )}
+        </div>
+      )}
+
+      {/* Mobile Menu */}
+      <div
+        className={`fixed top-0 left-0 h-full bg-white z-40 p-6 transition-transform transform ${
+          isMobileMenuOpen ? "translate-x-0 visible opacity-100" : "-translate-x-full invisible opacity-0"
+        }`}
+        style={{ width: "70%" }}
+      >
+        <div className="mt-16 border-t-2 pt-8">
+          {navLinks.map((link, index) => (
+            <div key={index} className="mb-4">
+              <div className="flex">
+              <div
+                className="text-md border-b-2 w-full pb-2 gap-20 font-medium flex items-center justify-between cursor-pointer"
+                onClick={() => toggleDropdown(index)}
+              >
+                {link.name}
+                <span>
+                <MdOutlineKeyboardArrowRight
+                size={20}
+                className={`transition-transform ${
+                  openDropdown === index ? "rotate-90" : ""
+                  }`}
+                  />
+                  </span>
+                </div>
+              </div>
+              {openDropdown === index && (
+                <ul className="mt-2 space-y-2 border-b-2 py-1">
+                  {link.subItems.map((subItem, idx) => (
+                    <li key={idx} className="pl-4 text-gray-600">
+                      {subItem}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="pt-40 ">
+              <p className="text-gray-600 text-sm flex items-center border-t-2 pt-4">
+                <div>
+                <MdOutlinePhoneInTalk size={24} className="mr-2 text-blue-600 " /> 
+                </div>
+                <div> 
+                <span className="text-black font-medium">Need help?</span><br/>
+                <a href="tel:6289000014" className="text-sm text-blue-500">Contact us on 6289000014</a>
+                </div>
+              </p>
+            </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
